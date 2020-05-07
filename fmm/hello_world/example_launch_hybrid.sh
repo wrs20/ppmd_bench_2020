3e38d8fb46eb7b645922634db02e8052ae914860
Will Saunders <wrs20@users.noreply.github.com>
2020-05-07 14:45:53 +0100

    Update example_launch_hybrid.sh

--- fmm/hello_world/example_launch_hybrid.sh
+++ fmm/hello_world/example_launch_hybrid.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-export OMP_NUM_THREADS=4
-mpirun -n 4 --bind-to socket python ../fmm_strong_scaling.py ../resource_files ./hello_world.json
+
+# For 2 nodes with 2 x 8 core cpus cores 
+export OMP_NUM_THREADS=2
+mpirun -ppn 8 -np 16  python ../fmm_strong_scaling.py ../resource_files ./hello_world.json
